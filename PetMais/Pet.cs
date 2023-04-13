52395c085e333d446603ee8d98e88e91feec558a
Andre Messias <andremessias.az@gmail.com>
2023-04-13 08:15:33 -0300

    implementando os atributos da classe principal

---
+++ PetMais/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetMais
+{
+	public class Pet
+	{
+		public int Id { get;  }
+		public string Nome { get; set; }
+		public string Tipo { get; set; }
+		public string Cor { get; set;}
+		public DateTime HorarioDeCadastro { get; set; }
+	}
+}
