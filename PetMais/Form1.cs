2ab25f1db522eb65f8676b3d501ca453df14002a
Andre Messias <andremessias.az@gmail.com>
2023-04-13 12:07:11 -0300

    implementado a listagem estatica no datagridview

--- PetMais/Form1.cs
+++ PetMais/Form1.cs
@@ -1,3 +1,5 @@
+using PetMais.Enums;
+
 namespace PetMais
 {
 	public partial class Form1 : Form
@@ -5,6 +7,15 @@ namespace PetMais
 		public Form1()
 		{
 			InitializeComponent();
+			var listaPets = new List<Pet>();
+			listaPets.AddRange(new[]
+			{
+				new Pet(1, "Junior", "Gato", "Preto", SexoPet.MASCULINO, DateTime.Now, DateTime.Now),
+				new Pet(2, "Max", "Cachorro", "Caramelo", SexoPet.MASCULINO, DateTime.Now, DateTime.Now),
+				new Pet(3, "Judite", "Cachorro", "Branco", SexoPet.FEMININO, DateTime.Now, DateTime.Now)
+			});
+
+			this.dataGridView1.DataSource = listaPets;
 		}
 	}
 }
