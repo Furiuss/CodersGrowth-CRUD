96ff49b1fda0aa274c6943ec33e8205bfaae1c82
Andre Messias <andremessias.az@gmail.com>
2023-04-18 14:29:49 -0300

    criado classe singleton para listagem de pets

---
+++ PetMais/Services/ListaDePets.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetMais.Services
+{
+	public sealed class ListaDePets
+	{
+		private static ListaDePets instancia = new ListaDePets();
+		private List<Pet> Pets = new List<Pet>();
+
+		private ListaDePets() { }
+
+		public static ListaDePets GetInstance()
+		{
+			{
+				if (instancia == null)
+				{
+					instancia = new ListaDePets();
+				}
+
+				return instancia;
+			}
+		}
+
+		public List<Pet> PegarListaDePets()
+		{
+			return Pets;
+		}
+
+		public Pet PegarPetPeloId(int id)
+		{
+			Pet pet = Pets.FirstOrDefault(i => i.Id == id);
+
+			if (pet == null)
+			{
+				return null;
+			}
+
+			return pet;
+		}
+
+		public void AdicionarPet(Pet pet)
+		{
+			pet.Id = AutoIncrementoDeId();
+			pet.DataDeCadastro = DateTime.Now;
+			Pets.Add(pet);
+		}
+
+		public void RemoverPet(Pet pet)
+		{
+			Pets.Remove(pet);
+		}
+
+		public void EditarPet(Pet pet)
+		{
+			int indice = Pets.IndexOf(pet);
+			Pets[indice] = pet;
+		}
+
+		private int AutoIncrementoDeId()
+		{
+			if (Pets.Count > 0)
+			{
+				return Pets.Max(pet => pet.Id) + 1;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+	}
+}
