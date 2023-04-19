9197df159487c9997ac660ef4092435c193156ed
Andre Messias <andremessias.az@gmail.com>
2023-04-19 14:07:11 -0300

    criado a migracao

---
+++ PetMais/Migrations/CriacaoDePets.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetMais.Migrations
+{
+	[Migration(20230419131240)]
+	public class CriacaoDePets : Migration
+	{
+
+		public override void Up()
+		{
+			Create.Table("Pet")
+				.WithColumn("Id").AsInt64().PrimaryKey().Identity()
+				.WithColumn("Nome").AsString(20).NotNullable()
+				.WithColumn("Tipo").AsString(20).NotNullable()
+				.WithColumn("Cor").AsString(20).NotNullable()
+				.WithColumn("Sexo").AsString(15).NotNullable()
+				.WithColumn("DataDeNascimento").AsDateTime().NotNullable()
+				.WithColumn("DataDeCadastro").AsDateTime().NotNullable();
+		}
+
+		public override void Down()
+		{
+			Delete.Table("Pets");
+		}
+	}
+}
--- PetMais/Program.cs
+++ PetMais/Program.cs
@@ -1,17 +1,45 @@
+using System;
+using System.Linq;
+
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+using PetMais.Migrations;
+
 namespace PetMais
 {
 	internal static class Program
 	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main()
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
 			Application.Run(new TelaDeVisualizacao());
+
+			using (var serviceProvider = CreateServices())
+			using (var scope = serviceProvider.CreateScope())
+			{
+				UpdateDatabase(scope.ServiceProvider);
+			}
+		}
+
+		private static ServiceProvider CreateServices()
+		{
+			return new ServiceCollection()				
+				.AddFluentMigratorCore()
+				.ConfigureRunner(rb => rb
+					.AddSqlServer()
+					.WithGlobalConnectionString("Data Source=DESKTOP-GMPDDMC")
+					.ScanIn(typeof(CriacaoDePets).Assembly).For.Migrations())
+				.AddLogging(lb => lb.AddFluentMigratorConsole())
+				.BuildServiceProvider(false);
+		}
+
+		private static void UpdateDatabase(IServiceProvider serviceProvider)
+		{
+			var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+			runner.MigrateUp();
 		}
 	}
 }
