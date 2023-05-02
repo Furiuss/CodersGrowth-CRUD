502bfa524054fb5e627d77019055fc37e15a51d7
Andre Messias <andremessias.az@gmail.com>
2023-05-02 11:49:37 -0300

    adicionado e testado o basico de uma aplicacao frontend web integrada com SAPUI5

---
+++ PetMais.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetMais.Web.Controllers
+{
+	[ApiController]
+	[Route("")]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+			return PhysicalFile(filePath, "text/html");
+		}
+	}
+}
