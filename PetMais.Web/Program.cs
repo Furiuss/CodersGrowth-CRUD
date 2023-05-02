7a1d4159811de8007c603f9383962892012ad299
Andre Messias <andremessias.az@gmail.com>
2023-05-02 12:47:44 -0300

    removido bin e obj

--- PetMais.Web/Program.cs
+++ PetMais.Web/Program.cs
@@ -1,10 +1,40 @@
+using Microsoft.AspNetCore.StaticFiles;
 using PetMais.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-startup.Configure(app, app.Environment);
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+	ServeUnknownFileTypes = true,
+	DefaultContentType = "text/plain;charset=utf-8",
+	ContentTypeProvider = new FileExtensionContentTypeProvider(new Dictionary<string, string>
+				{
+					{".properties", "text/plain;charset=utf-8" }
+				})
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 app.Run();
