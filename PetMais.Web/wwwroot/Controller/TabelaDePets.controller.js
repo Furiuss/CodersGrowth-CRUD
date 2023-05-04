776f92ad37e4fd41a8eaf17c99077608ae4337f6
Andre Messias <andremessias.az@gmail.com>
2023-05-04 10:16:13 -0300

    adicionado e testado novas funcionalidades

--- PetMais.Web/wwwroot/Controller/TabelaDePets.controller.js
+++ PetMais.Web/wwwroot/Controller/TabelaDePets.controller.js
@@ -2,40 +2,38 @@ sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    // "../model/formatter",
-    // "sap/ui/model/Filter",
-    // "sap/ui/model/FilterOperator",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
   function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.petmais.controller.TabelaDePets", {
+      formatter: formatter,
       onInit: function () {
         var oModel = new JSONModel();
         fetch("/api/pets")
           .then(data => data.json())
-          .then(data => oModel.setData({pets : data}))
-      
+          .then(data => oModel.setData({ pets: data }))
+
         console.log(oModel)
         this.getView().setModel(oModel)
-        // var oViewModel = new JSONModel({
-        //   currency: "EUR",
-        // });
-        // this.getView().setModel(oViewModel, "view");
-      }
-      // onFilterInvoices: function (oEvent) {
-      //   var aFilter = [];
-      //   var sQuery = oEvent.getParameter("query");
-      //   if (sQuery) {
-      //     aFilter.push(
-      //       new Filter("ProductName", FilterOperator.Contains, sQuery)
-      //     );
-      //   }
 
-      //   var oList = this.byId("invoiceList");
-      //   var oBinding = oList.getBinding("items");
-      //   oBinding.filter(aFilter);
-      // },
+      },
+      aoPesquisar: function (oEvent) {
+        var aFilter = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("nome", FilterOperator.Contains, sQuery)
+          );
+        }
+
+        var oList = this.byId("petsTable");
+        var oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
+      },
       // onPress: function (oEvent) {
       //   var oItem = oEvent.getSource();
       //   var oRouter = this.getOwnerComponent().getRouter();
